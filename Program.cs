7fb77347381018b7d8e33518263909b26b305d4e
IvanovDmitr <146060514+IvanovDmitr@users.noreply.github.com>
2024-04-26 16:11:33 +0300

    КР_2 нормальный файл

--- Program.cs
+++ Program.cs
@@ -1,196 +1,148 @@
-﻿using System;
-static class Hello
+﻿
+using System;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Linq;
+public class Task
 {
-
-    //задание 3.4
-
-    public class Program
+    string text;
+    public Task(string text)
     {
-        public class Sportsmen
+        this.text = text;
+    }
+}
+public class Task_2 : Task
+{
+    string text;
+    public Task_2(string text) : base(text)
+    {
+        this.text = text;
+    }
+    public override string ToString()
+    {
+        return text;
+    }
+    public string ReverseWords()
+    {
+        StringBuilder result = new StringBuilder();
+        string[] words = text.Split(' ');
+
+        int wordIndex = 0;
+        foreach (char c in text)
         {
-            string name;
-            double resultss;
-            public Sportsmen(string name_, double results_)
+            if (char.IsLetterOrDigit(c))
             {
-                name = name_;
-                resultss = results_;
+                result.Insert(result.Length - wordIndex, c);
             }
-            public double GetResult()
+            else
             {
-                return resultss;
-            }
-            public string Name { get { return name; } }
-            public double Resultss { get { return resultss; } }
-            public void InfoP()
-            {
-                Console.WriteLine($"имя: {Name} результат: {resultss} ");
-            }
-            public static Sportsmen[] AllGroup(Sportsmen[] group1, Sportsmen[] group2)
-            {
-                Sportsmen[] group = new Sportsmen[group1.Length + group2.Length];
-                for (int i = 0; i < group1.Length; i++)
-                {
-                    group[i] = group1[i];   
-                }
-                for (int i = 0;i < group2.Length; i++)
-                {
-                    group[i + group2.Length] = group2[i];
-                }
-                return group;
+                result.Append(c);
+                wordIndex = 0;
             }
 
-            public static Sportsmen[] MergeSort(Sportsmen[] array)
+            if (wordIndex < words.Length )
             {
-                if (array.Length < 2)
-                {
-                    return array;
-                }
-
-                int mid = array.Length / 2;
-                Sportsmen[] left = new Sportsmen[mid];
-                Sportsmen[] right = new Sportsmen[array.Length - mid];
-
-                for (int i = 0; i < mid; i++)
-                {
-                    left[i] = array[i];
-                }
-                for (int i = mid; i < array.Length; i++)
-                {
-                    right[i - mid] = array[i];
-                }
-                MergeSort(left); MergeSort(right);
-                Merge(array, left, right);
-                return array;
+                result.Append(' ');
+                wordIndex++;
             }
 
-            public static void Merge(Sportsmen[] targetarray, Sportsmen[] array1, Sportsmen[] array2)
-            {
-                int array1minindex = 0;
-                int array2minindex = 0;
-
-                int targetarrayminindex = 0;
-
-                if (array1[array1minindex].resultss >= array2[array2minindex].resultss)
-                {
-                    targetarray[targetarrayminindex] = array1[array1minindex];
-                    array1minindex++;
-                }
-                else
-                {
-                    targetarray[targetarrayminindex] = array2[array2minindex];
-                    array2minindex++;
-                }
-                targetarrayminindex++;
-
-                while (array1minindex < array1.Length)
-                {
-                    targetarray[targetarrayminindex] = (array1[array1minindex]);
-                    array1minindex++;
-                    targetarrayminindex++;
-                }
-
-                while (array2minindex < array2.Length)
-                {
-                    targetarray[targetarrayminindex] = (array2[array2minindex]);
-                    array2minindex++;
-                    targetarrayminindex++;
-                }
-            }
+            wordIndex++;
         }
 
-        class Mskiers : Sportsmen
+        return result.ToString();
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ввести текст:");
+        Task_2 processor = new Task_2(Console.ReadLine());
+        string reversedText = processor.ReverseWords();
+        Console.WriteLine("Перевернутый текст:");
+        Console.WriteLine(reversedText.ToString());
+
+        Console.WriteLine("Ввести два слова через пробел:");
+        string inputText = Console.ReadLine();
+
+        Task_1 finder = new Task_1(inputText);
+        finder.FindCommonLetters();
+
+
+        //TestFolderManager manager = new TestFolderManager();
+
+        //manager.CreateTaskFile(1, "{\"task\": \"One\"}");
+        //manager.CreateTaskFile(2, "{\"task\": \"Two\"}");
+
+
+    }
+    class Task_1 : Task
+    {
+        string word1;
+        string word2;
+        string text;
+        public Task_1(string Text) : base(Text)
         {
-            int result;
-            public Mskiers(string name_, int results_) : base(name_, results_)
+            this.text = Text;
+            string[] words = Text.Split(' ');
+
+            if (words.Length != 2)
             {
-                result = results_;
+                Console.WriteLine("Не два слова введено");
+                return;
             }
 
+            word1 = words[0];
+            word2 = words[1];
+        }
+        public override string ToString()
+        {
+            return text;
         }
 
-        class Wskiers : Mskiers
+        public void FindCommonLetters()
         {
-            int results;
-            public Wskiers(string name_, int results_) : base(name_, results_)
+            var commonLetters = word1.Intersect(word2);
+
+            foreach (char letter in commonLetters)
             {
-                results = results_;
+                Console.Write(letter + "\n");
             }
-        }
-
-        static void Sort(Sportsmen[] list)
-        {
-            for (int i = 0; i < list.Length; i++)
-            {
-                for (int j = i; j < list.Length; j++)
-                    if (list[i].GetResult() < list[j].GetResult())
-                    {
-                        Sportsmen person_now = list[j]; // меняем игроков 
-                        list[j] = list[i];
-                        list[i] = person_now;
-                    }
-            }
-        }
-        public static void Main(string[] args)
-        {
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
-            Mskiers[] group1 = new Mskiers[n1];
-            Wskiers[] group2 = new Wskiers[n2];
-            //Sportsmen[] allgroup = Program.Sportsmen.AllGroup(group1, group2);
-
-            for (int i = 0; i < n1; i++)
-            {
-                string name_ = Console.ReadLine();
-                int result_ = int.Parse(Console.ReadLine());
-
-                Mskiers Person = new Mskiers(name_, result_);
-
-                group1[i] = Person;
-                //allgroup[i] = Person;
-            }
-
-            for (int i = 0; i < n2; i++)
-            {
-                string name_ = Console.ReadLine();
-                int result_ = int.Parse(Console.ReadLine());
-
-                Wskiers Person = new Wskiers(name_, result_);
-
-                group2[i] = Person;
-                //allgroup[i + n1] = Person;
-            }
-
-            Sort(group1);
-            Sort(group2);
-            Sportsmen[] allgroup = Program.Sportsmen.AllGroup(group1, group2);
-            allgroup = Program.Sportsmen.MergeSort(allgroup);
-
-            Console.WriteLine("1 группа:");
-            for (int i = 0; i < n1; i++)
-            {
-                Console.Write($"{i + 1} место:"); ;
-                group1[i].InfoP();
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("2 группа:");
-            for (int i = 0; i < n2; i++)
-            {
-                Console.Write($"{i + 1} место:"); ;
-                group2[i].InfoP();
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Все участники:");
-            for (int i = 0; i < n1 + n2; i++)
-            {
-                Console.Write($"{i + 1} место:"); ;
-                allgroup[i].InfoP();
-                Console.WriteLine();
-            }
-            Console.WriteLine();
         }
     }
+
+    //class TestFolderManager
+    //{
+    //    private string desktopPath;
+    //    private string testFolderPath;
+
+    //    public TestFolderManager()
+    //    {
+    //        desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+    //        testFolderPath = Path.Combine(desktopPath, "Test");
+
+    //        if (!Directory.Exists(testFolderPath))
+    //        {
+    //            Directory.CreateDirectory(testFolderPath);
+    //        }
+    //    }
+
+    //    public void CreateTaskFile(int taskId, string taskData)
+    //    {
+    //        string taskFilePath = Path.Combine(testFolderPath, "task_" + taskId + ".json");
+
+    //        if (File.Exists(taskFilePath))
+    //        {
+    //            string existingData = File.ReadAllText(taskFilePath);
+    //            Console.WriteLine("Data from existing file " + Path.GetFileName(taskFilePath) + ": " + existingData);
+    //        }
+    //        else
+    //        {
+    //            File.WriteAllText(taskFilePath, taskData);
+    //            Console.WriteLine(Path.GetFileName(taskFilePath) + taskData);
+    //        }
+    //    }
+    //}
 }
