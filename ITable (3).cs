817d22883e9f99c225f55c44d14a80258e4a2fa7
IvanovDmitr <146060514+IvanovDmitr@users.noreply.github.com>
2024-05-30 19:27:30 +0300

    Add files via upload
    Мы сделали все диаграммы кроме состояний. Нужно отправлять те две, если одной не достаёт?

---
+++ ITable (3).cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ЛР_10_1
+{
+    public interface ITable
+    {
+        void AddMatch(Team team1, Team team2);
+        void Sort(string criteria);
+        void Sort(int dummy);
+    }
+}
---
+++ TournamentTable2 (3).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ЛР_10_1
+{
+    public partial class TournamentTable
+    {
+        public Team GetBest()
+        {
+            Teams.Sort((x, y) => y.CalculatePoints().CompareTo(x.CalculatePoints()));
+            return Teams[0];
+        }
+
+        public List<Team> GetBest(int n)
+        {
+            Teams.Sort((x, y) => y.CalculatePoints().CompareTo(x.CalculatePoints()));
+            return Teams.GetRange(0, Math.Min(n, Teams.Count));
+        }
+    }
+}
